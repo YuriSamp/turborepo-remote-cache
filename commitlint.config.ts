befd39a4db49423a8b53656d5fbde1fef0cbc035
Yuri <yurisamp123@gmail.com>
2024-04-15 00:32:45 -0300

    feat: add husky

---
+++ commitlint.config.ts
@@ -0,0 +1,10 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "chore", "fix", "refactor", "ci", "docs"],
+    ],
+  },
+};
